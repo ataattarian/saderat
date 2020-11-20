c9eca85df748cc9fe0937dc813b974e6097237b9
Ata Attarian <ata.attarian78@gmail.com>
2020-11-20 13:55:37 +0330

    add function get

--- saderat.cpp
+++ saderat.cpp
@@ -6,12 +6,13 @@ using name space std;
 clas saderat{
 public:
   ///////////////تابع سازنده///////////////
-  saderat(char n,int c, float w,float p){
+  saderat(char n='n',int c=0, float w=0,float p=0){
     name=n;
     code_saderat=c;
     weight=w;
     price=p;
   }
+  void get(char n,int c, float w,float p);
 
 
 
@@ -21,3 +22,11 @@ private:
   float weight;
   float price;
 };
+
+///////////داده ها رو از کاربر گرفته و ذخیره میکنه/////////////
+void saderat::get(char n,int c, float w,float p){
+  name=n;
+  code_saderat=c;
+  weight=w;
+  price=p;
+}
