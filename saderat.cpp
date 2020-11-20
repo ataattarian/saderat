4ae6e3ddc3a22a833bec4bf5c9be083235e95419
Ata Attarian <ata.attarian78@gmail.com>
2020-11-20 13:35:52 +0330

    creat class saderat

--- saderat.cpp
+++ saderat.cpp
@@ -1,3 +1,18 @@
 #include<iostream>
 #include<conio.h>
+#include<string>
+#include<strin.h>
 using name space std;
+
+
+clas saderat{
+public:
+  
+
+
+private:
+  string name;
+  int code_sadert;
+  float weight;
+  float price;
+};
